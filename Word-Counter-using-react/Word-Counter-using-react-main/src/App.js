089b3ff83d9a6c5f096edd3190dcfcf22d06445f
Khushbu <83541710+khushbu4066@users.noreply.github.com>
2023-10-26 09:08:00 +0530

    Add files via upload

---
+++ Word-Counter-using-react/Word-Counter-using-react-main/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [text, setText] = useState('');
+  const wordCount = text.trim().split(/\s+/).length;
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div >
+      <h1>Live Word Count</h1>
+      <textarea
+        rows="5"
+        cols="50"
+        placeholder="Type something..."
+        value={text}
+        onChange={handleChange}
+      />
+      <p>Word Count: {wordCount}</p>
+    </div>
+  );
+}
+
+export default App;
